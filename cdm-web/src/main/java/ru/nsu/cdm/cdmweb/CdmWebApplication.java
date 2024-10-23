162e6f37dfed6804f67de6ac4501e56507ba0859
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2024-10-23 21:00:04 +0700

    Add main directory for backend

---
+++ cdm-web/src/main/java/ru/nsu/cdm/cdmweb/CdmWebApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.cdm.cdmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdmWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdmWebApplication.class, args);
+    }
+
+}
---
+++ cdm-web/src/test/java/ru/nsu/cdm/cdmweb/CdmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.cdm.cdmweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdmWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
