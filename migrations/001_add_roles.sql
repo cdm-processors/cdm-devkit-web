c23e2f4cb5f2945a7e49afc19fd71810484e4c80
Artyom Chuvashov <a.chuvashov@g.nsu.ru>
2025-05-12 09:45:42 +0700

    Implement goose for migrations and make sql idempotent

--- migrations/001_add_roles.sql
+++ migrations/001_add_roles.sql
@@ -1,2 +1,10 @@
+-- +goose Up
 INSERT INTO public.t_roles(id, name)
-VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
+SELECT 1, 'ROLE_USER'
+WHERE NOT EXISTS (SELECT 1 FROM public.t_roles WHERE id = 1)
+UNION ALL
+SELECT 2, 'ROLE_ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM public.t_roles WHERE id = 2);
+
+-- +goose Down
+DELETE FROM public.t_roles WHERE id IN (1, 2);
