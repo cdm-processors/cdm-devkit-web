7b637d26bc3ff2035ab615e0eb331fc9d90be31c
dromankin <d.romankin@g.nsu.ru>
2025-05-25 22:29:15 +0700

    Fix test fail

--- cdm-web/src/test/java/org/cdm/web/backend/CdmWebApplicationTests.java
+++ cdm-web/src/test/java/org/cdm/web/backend/CdmWebApplicationTests.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+
 @SpringBootTest
 @ContextConfiguration(classes = CdmWebApplication.class)
 @ImportResource({"application.properties"})
